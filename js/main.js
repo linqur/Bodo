39385850caef6d5a812c05b05512f67b32c6ecdf
Linqur <Linqur@gmail.com>
2017-03-31 16:08:59 +0300

    -

---
+++ js/main.js
@@ -0,0 +1,6 @@
+document.getElementById('nav-open').onclick=function navi_vision() {
+  document.getElementById('navigator').className='navbar nav-menu nav-active';
+}
+document.getElementsByClassName('nav-close')[0].onclick=function navi_vision() {
+  document.getElementById('navigator').className='navbar nav-menu';
+}
